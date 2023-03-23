6177ea434e75948aee2240094deaa685558cc84c
KfNGOe <zinzendorf-app@github.io>
2023-03-23 17:11:51 +0000

    Update from https://github.com/KfNGOe/zinzendorf-app/commit/fdec88351286e106fca6e0aa8caa90ee13efdc8e

--- js/index.js
+++ js/index.js
@@ -17,47 +17,7 @@
         var click_lang = $(this);
         console.log(click_lang.attr("lang"));
         langDeEn(click_lang.attr("lang"));
-    }) ;
+    }) ;    
     
-    $( "ul.submenu-start li.list-item a" ).click(function() {
-        var click = $( this );
-        var textId = "#text #" + click.attr("id") + "_text";
-        var itemId = "ul.submenu-start li.list-item #" + click.attr("id");
-        
-        $( "div.text" ).hide();
-        $( textId ).toggle();
-        
-        $("ul.submenu-start li.list-item a").removeClass("active");
-        $( itemId ).addClass("active");
-    });
 
-    $( "#korrFerd_text .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;
-    });
-
-    $( "#digEd_text .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;
-    });
-
-    $( "#erschBaende_text .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;
-    });
-    
-    $( "#verzBuech_text .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;
-    });
-    
-    $( "ul.list-letters li.list-item .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;                    
-    }) ;
-
-    $( "ul.list-texts li.list-item .icon-texts a" ).click(function() {
-        var click = $( this );
-        embedLoad( click ) ;
-    });
     
